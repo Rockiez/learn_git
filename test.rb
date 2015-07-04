21c77200b0de2ae8d9f0cf62a53795c63b14f45d
Rockiez <simpi@foxmail.com>
2015-07-04 22:18:20 +0800

    ex5_pro_short_ver

--- test.rb
+++ test.rb
@@ -1,8 +1,6 @@
-def power_of(n)
-	lambda do |x|
-		return x ** n
-	end
-end
-
-cube = power_of 3
-p cube.call(5)
+prc = Proc.new{|a, b| a + b}
+p prc.call(1, 2)
+p prc[3, 4]
+p prc.yield(5,6)
+p prc.(7, 8)
+p prc === [9, 10]
