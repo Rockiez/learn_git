c4e436f97a91e2d6522a2e04a483d3852f777e26
Rockiez <simpi@foxmail.com>
2015-07-04 18:09:55 +0800

    ex2_leap_Proc

--- test.rb
+++ test.rb
@@ -1,10 +1,8 @@
-hello1 = Proc.new do |name|
-	puts "Hello #{name}"
+leap = proc do |year|
+	year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+	#必须被4整除，不被100整除，但可以被400整除。
 end
 
-hello2 = proc do |name|
-	puts "Hello #{name}"
-end
-
-hello1.call("world")
-hello2.call("world")
+p leap.call(2000)
+p leap.call(2014)
+p leap.call(1500)
