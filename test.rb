ccf830c29d0f025c6401ef43e0965ef6b855e0f0
Rockiez <simpi@foxmail.com>
2015-07-04 19:00:17 +0800

    ex4_lambda_return

--- test.rb
+++ test.rb
@@ -1,8 +1,8 @@
-leap = proc do |year|
-	year % 4 == 0 && year % 100 != 0 || year % 400 == 0
-	#必须被4整除，不被100整除，但可以被400整除。
+def power_of(n)
+	lambda do |x|
+		return x ** n
+	end
 end
 
-p leap.call(2000)
-p leap.call(2014)
-p leap.call(1500)
+cube = power_of 3
+p cube.call(5)
